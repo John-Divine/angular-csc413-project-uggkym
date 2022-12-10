bb529120a9082634e1e0c27d0fa312f2445edba2
John-Divine <70081718+John-Divine@users.noreply.github.com>
2022-12-10 03:55:52 -0700

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StudentComponent } from './student/student.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, StudentComponent ],
+  declarations: [ AppComponent, StudentComponent,StudentDetailsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Student} from '../student';
 import { STUDENTS} from '../mock-students';
+import { StudentService } from '../student.service';
 
 @Component({
   selector: 'app-student',
@@ -8,15 +9,19 @@ import { STUDENTS} from '../mock-students';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  students =STUDENTS;
+  students:Student[]=[];
   selectedStudent?: Student;
   onSelect(student:Student): void {
   this.selectedStudent = student;
 }
 
-  constructor() { }
+  constructor(private studentService: StudentService) {}
+  getStudents(): void {
+    this.students = this.studentService.getStudents();
+  }
 
-  ngOnInit() {
+  ngOnInit():void{
+    this.getStudents()
   }
 
 }
